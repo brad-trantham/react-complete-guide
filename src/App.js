88a42c6fc52c878b414b215404bdc69362cdad59
Brad Trantham <trantham@gmail.com>
2020-02-14 10:54:46 -0600

    Error catching example.
    Doesn't do much in dev mode but in production he says it gives a nice way to display messages for caught errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -50,10 +51,12 @@ class App extends Component {
         persons = (
           <div>
             {this.state.persons.map((person, index) => {
-              return <Person name={person.name} age={person.age}
+              return <ErrorBoundary>
+                        <Person name={person.name} age={person.age}
                              click={()=>this.deletePersonHandler(index)}
                              key={person.id}
                              changed={(event)=>this.nameChangedHandler(event, person.id)}/>
+                      </ErrorBoundary>
             })}
           </div>
         );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -24,6 +24,12 @@ const person = (props) => {
         }
     }
 
+    const rnd = Math.random();
+
+    if( rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
+
     return(
         //<div className="Person" style={style}>
         <StyledDiv>
